4d27d01a60541b24705674891b87188aa2e2ff32
marcin.jedynski <marcin.jedynski@cognifide.com>
2019-04-02 14:44:55 +0200

    comment progress

--- extract.rb
+++ extract.rb
@@ -1,8 +1,6 @@
-
-
 file = File.open('084.xor', 'rb')
 
-# p file.bytes.each_slice(256).take(41).combination(2).to_a.length
+#p file.bytes.each_slice(256).take(41).combination(2).to_a.length
 #puts file.bytes.each_slice(256).take(41).combination(2).map{|slice| slice.transpose.map{|x| x.reduce(){|a,b| a^b }}.pack("c*") }
 
 slices = file.bytes.each_slice(256).take(41)
@@ -14,6 +12,6 @@ pairs = slices.map{|x| [x,chosen]}
 
 xored = pairs.map{|slice| slice.transpose.map{|x| x.reduce(){|a,b| a^b }} }
 filtered = xored.map{|x| x.each_with_index.map{|val,index| [val,index]}.select{|element| (possible_space_range_1 === element[0] or possible_space_range_2 === element[0] or element[0] == 0)}}
+
+#prints possible candidates for spaces for first chunk of message
 filtered.each{|elem| p elem}
-# p filtered[1]
-# p filtered[2]
